7f2ccecdd609069a88e8a161dbab35551a240aef
Jennifer Stoos <jstoos@bitlaw.com>
2018-06-23 19:46:45 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ module FormsLab
 
     get '/' do
       <h1>Welcome to the Nested Forms Lab!</h1>
-      <h1>let's navigate to the '/new'</h1>
+      <h1>"let's navigate to the '/new'"</h1>
       erb :new
     end
 
