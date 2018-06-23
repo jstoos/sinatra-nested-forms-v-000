c4f4d72e0cd553a048b2191fdcb9bd7f74bb0664
Jennifer Stoos <jstoos@bitlaw.com>
2018-06-23 20:26:42 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -13,11 +13,11 @@ module FormsLab
 
     post '/pirates' do
       @pirate = Pirate.new(params[:pirate])
-      
+
       params[:pirate][:ships].each do |details|
         Ship.new(details)
       end
-      
+
       @ships = Ship.all
       erb :show
     end
