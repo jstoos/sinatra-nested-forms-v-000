2e6a15eff44ada0cc2f596fbea4d02689759f03b
Jennifer Stoos <jstoos@bitlaw.com>
2018-06-23 18:55:16 +0000

    automatically backed up by learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,12 @@
 class Ship
+
+  attr_accessor :name, :type, :booty
+
+  SHIPS = {}
+
+  def initialize(params)
+    @name = params[:name]
+    @type = params[:type]
+    @booty = = params[:booty]
+  
 end
